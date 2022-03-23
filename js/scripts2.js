a891da5af286c9bf9b6cc1139796e7bbd7b96731
BriChavez <brianachavez@gmail.com>
2022-03-23 13:56:16 -0700

    give my partners credit
    Co-Authored-by Jeremy Martin <jeremyvs1945@gmail.com>
    Co-Authored-by Marcus Padilla <marcanthonyzpadilla@gmail.com>

---
+++ js/scripts2.js
@@ -0,0 +1,10 @@
+$(document).ready(function () { 
+  $("#formOne").submit(function(event) {
+
+    const exclamationInput = $("input#exclamation").val().toUpperCase();
+    $(".exclamation").text(exclamationInput);
+    $("#story").show();
+
+    event.preventDefault();
+  });
+})
